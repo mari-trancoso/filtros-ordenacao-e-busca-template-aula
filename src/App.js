42d160226e805c0860f7cd560ccba506665ee468
mari-trancoso <mnegraotrancoso@gmail.com>
2022-10-27 11:13:53 -0300

    exercicio da manhã

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import pokemons from "./pokemon/pokemon.json";
 import PokemonCard from "./components/PokemonCard/PokemonCard";
 import { getColors } from "./utils/ReturnCardColor";
 import Header from "./components/Header/Header.js";
+import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+
 const GlobalStyle = createGlobalStyle`
   *{
     padding: 0;
@@ -18,18 +21,65 @@ const CardsContainer = styled.div`
   justify-items: center;
 `;
 function App() {
+
+  const [buscaId, setBuscaId] = useState("")
+  const [buscaNome, setBuscaNome] = useState("")
+  const [ordenaAlfabeto, setOrdenaAlfabeto] = useState("")
+  const [escolhePorTipo, setEscolhePorTipo] = useState("")
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header 
+        buscaId = {buscaId}
+        setBuscaId = {setBuscaId}
+        buscaNome = {buscaNome}
+        setBuscaNome = {setBuscaNome}
+        ordenaAlfabeto = {ordenaAlfabeto}
+        setOrdenaAlfabeto = {setOrdenaAlfabeto}
+        escolhePorTipo = {escolhePorTipo}
+        setEscolhePorTipo = {setEscolhePorTipo}
+        />
       <CardsContainer>
-        {pokemons.map((pokemon) => {
+        {pokemons
+        //-------------busca por id
+        .filter((pokemon) => {
+          return pokemon.id.includes(buscaId)
+          // if(pokemon.id.includes(buscaId)){return pokemon}
+        })
+        //-------------busca por nome
+        .filter((pokemon) => {
+          return pokemon.name.english.toLowerCase().includes(buscaNome.toLowerCase())
+        })
+        //.filter((pokemon)=> pokemon.id.includes(buscaId) && pokemon.name.english.toLowerCase().includes(buscaNome.toLowerCase()))//
+        //-------------ordena
+        .sort((a, b) => {
+          if(ordenaAlfabeto === "crescente"){
+            if(a.name.english < b.name.english){
+              return -1
+            } else {
+              return 1
+            }
+          } else if (ordenaAlfabeto === "decrescente") {
+            if(a.name.english > b.name.english){
+                return -1
+              } else {
+                return 1
+              }
+          }
+        })
+        //-------------tipo
+        .filter((pokemon) => {
+          return escolhePorTipo ? pokemon.type.includes(escolhePorTipo) : pokemon
+        })
+        .map((pokemon) => {
           return <PokemonCard
           cardColor={getColors(pokemon.type[0])}
           key={pokemon.id}
           pokemon={pokemon}
         />
         })}
+        
       </CardsContainer>
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Header = () => {
+const Header = (props) => {
   const pokemontypesArray = [
     "Normal",
     "Fire",
@@ -23,16 +23,45 @@ const Header = () => {
     "Fairy",
   ];
 
+  const onChangeId = (e) => {
+    props.setBuscaId(e.target.value)
+  }
+
+  const onChangeNome = (e) => {
+    props.setBuscaNome(e.target.value)
+  }
+
+  const onChangeOrdenacao = (e) => {
+    props.setOrdenaAlfabeto(e.target.value)
+  }
+
+  const onChangeTipo = (e) => {
+    props.setEscolhePorTipo(e.target.value)
+  }
+
   return (
     <Container>
-      <input type="number" placeholder="Buscar por id" />
-      <input type="text" placeholder="Buscar por nome" />
-      <select>
+      <input 
+        type="number" 
+        placeholder="Buscar por id" 
+        value = {props.buscaId}
+        onChange={onChangeId}/>
+      <input 
+        type="text" 
+        placeholder="Buscar por nome"
+        value={props.buscaNome}
+        onChange={onChangeNome}
+         />
+      <select value={props.ordenaAlfabeto} onChange = {onChangeOrdenacao}>
         <option value="">Ordenar</option>
-        <option value="">Crescente</option>
-        <option value="">Decrescente</option>
+        <option value="crescente">Crescente</option>
+        <option value="decrescente">Decrescente</option>
       </select>
-      <select name="tipo" id="tipo">
+      <select 
+        name="tipo" 
+        id="tipo"
+        value={props.escolhePorTipo}
+        onChange={onChangeTipo}>
         <option value="">Selecione um tipo</option>
         {pokemontypesArray.map((type) => {
           return (
